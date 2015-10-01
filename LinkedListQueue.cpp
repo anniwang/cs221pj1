63ab6a70dfbf6d58727dba437f0ff7036ccaf552
Gui Xuan Zhao <d7c9@deas.ugrad.cs.ubc.ca>
2015-10-01 14:28:04 -0700

    Adding LinkedListQ

--- LinkedListQueue.cpp
+++ LinkedListQueue.cpp
@@ -7,32 +7,48 @@
 #include <cassert>
 #include <iostream>
 
-
-/*constructor*/
 LinkedListQueue::LinkedListQueue()
 {
-  // TODO
+	head = NULL;
+	tail = NULL;
 }
 
 void LinkedListQueue::put_in(PuzzleState *elem)
 {
-  // TODO
+	if (is_empty()){
+		head = tail = new node;
+		head->next = NULL;
+		head->data = elem;
+	}	
+	else {
+		tail->next = new node;
+		tail->next = NULL;
+		tail->data = elem;
+	}
 }
 
 PuzzleState *LinkedListQueue::take_out()
 {
-  // TODO
+	assert(!is_empty());
+	PuzzleState* ret = head->data;
+	node* temp = head;
+	head = head->next;
+	delete temp;
+	return ret;
 }
 
 bool LinkedListQueue::is_empty()
 {
-  // TODO
+	return (head == NULL);
 }
 
-/*destructor*/
 LinkedListQueue::~LinkedListQueue()
 {
-  // TODO
+  	while (!is_empty()) {
+		node* temp = head;
+		head = head->next;
+		delete temp;
+	}
 }
 
 #endif
